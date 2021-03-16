b3f995d8d4f47a1269ea25d59122daf06979e42e
Fábio H. dos S. Rocha <55759993+FabioHSR@users.noreply.github.com>
2021-03-16 18:33:13 -0300

    Issue: #1 Get gas_stations with filters

--- src/controllers/GasStationController.ts
+++ src/controllers/GasStationController.ts
@@ -1,8 +1,49 @@
 import { Request, Response } from "express";
 import { getCustomRepository,Not,IsNull } from "typeorm";
+import { GasStationRepository } from "../repositories/GasStationRepository";
 import { UserStationRatesRepository } from "../repositories/UserStationRatesRepository";
 
 class GasStationController{
+
+    async list(request: Request, response: Response){
+        var {FU,city,district,distributor} = request.query;
+        console.log(request.query)
+
+        FU = FU ? FU : '-1';
+        city = city ? city : '-1';
+        district = district ? district : '-1';      
+        distributor = distributor ? distributor : '-1';
+
+        const gasStationRepository = getCustomRepository(GasStationRepository);
+
+        const gasStationList = await gasStationRepository
+        .createQueryBuilder("gas_station")
+        .where(
+        "(gas_station.FU = :FU OR '-1' = :FU)"+
+        "AND (gas_station.city = :city OR '-1' = :city)"+
+        "AND (gas_station.district = :district OR '-1' = :district)"+
+        "AND (gas_station.distributor = :distributor OR '-1' = :distributor)"
+        ,{
+            FU: FU, 
+            city: city, 
+            district: district,
+            distributor: distributor
+        })
+        .getMany();
+
+        if(!gasStationList){
+            return response.status(202).json({
+                message: 'Not found',
+                status: 404,
+            });
+        }
+
+        return response.status(200).json(
+            {
+            count: gasStationList.length,
+            data: gasStationList
+        })
+    }
 
     async execute(request: Request, response: Response){
         const {station_id} = request.params;
